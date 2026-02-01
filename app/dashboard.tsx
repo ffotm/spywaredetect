0ec0d329205168188c8f24ae80b66ae2030871d2
unknown <otmani.fad@gmail.com>
2026-02-01 21:27:31 +0100

    front

---
+++ app/dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './components/navbar'
+import AntispywareDashboard from './components/dash'
+
+
+const Dashboard = () => {
+    return (
+        <div>
+
+            <AntispywareDashboard />
+        </div>
+    )
+}
+
+export default Dashboard
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import React from "react";
+import "./globals.css";
+import Dashboard from "./dashboard";
+
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-
+      <Dashboard />
     </div>
   );
 }
